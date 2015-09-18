979244fd71190a7e4078799eeafb344bfbf9bc3f
Julian Gindi <juliangindi@gmail.com>
2015-09-18 10:26:16 -0400

    spawning docker processes in coroutine

--- build-api/main.go
+++ build-api/main.go
@@ -20,15 +20,17 @@ type DockerInfo struct {
 func buildDockerContainer() {
 	// We can pass in a callback here, or just handle the status update
 	// request from this function
-	command := "sudo docker build --no-cache=True --tags='franklin_builder_tmp:tmp' ."
-	// 1. Will need to expand this to make sure we are running the command
-	// in the style we want
-	if err := exec.Command(command).Run(); err != nil {
+	buildCommand := exec.Command("docker", "build", "--no-cache=True", "--tags='franklin_builder_tmp:tmp'", ".")
+	if err := buildCommand.Run(); err != nil {
 		fmt.Println(os.Stderr, err)
-		os.Exit(1)
 	}
-	// 2. Let's wait for and handle any errors after each shell command
-	// 3. Call 'tear_down_project' and remove tmp directory
+
+	tearDown := exec.Command("scripts/tear_down_project.sh")
+	if err := tearDown.Run(); err != nil {
+		fmt.Println(os.Stderr, err)
+	}
+
+	os.Remove("tmp/")
 }
 
 func main() {
