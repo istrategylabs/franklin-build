613be32500978233b8c5dab045501cbc8b114257
Julian Gindi <juliangindi@gmail.com>
2015-12-04 12:00:32 -0500

    removing delete directive from rsync script for now

--- scripts/rsync_project.sh
+++ scripts/rsync_project.sh
@@ -3,4 +3,4 @@
 fileToTransfer=$1
 remoteLocation=$2
 
-rsync -azIe "ssh -i /home/franklin/.ssh/id_rsa" --delete $fileToTransfer franklin@islstatic.com:$remoteLocation
+rsync -azIe "ssh -i /home/franklin/.ssh/id_rsa" $fileToTransfer franklin@islstatic.com:$remoteLocation
