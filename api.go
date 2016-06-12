bf8c89221a9a2394a39540cd25f07e200d1cba98
Edward Romano <oudeis.metis@gmail.com>
2016-06-11 20:34:51 -0400

    Path trimming bug

--- api.go
+++ api.go
@@ -248,11 +248,11 @@ func uploadProjectS3(ctx log.Interface, localPath, remoteLoc string) {
 
     // Setup for upload
 	uploader := s3manager.NewUploader(session.New(&aws.Config{Region: aws.String("us-east-1")}))
-    buildLoc = Config.BUILDLOCATION + "/"
+    buildLoc := Config.BUILDLOCATION + "/"
 
 	// For each file found walking upload it to S3.
 	for path := range walker {
-        trimmedPath = strings.Trim(path, buildLoc)
+        trimmedPath := strings.Trim(path, buildLoc)
 		rel, err := filepath.Rel(localPath, trimmedPath)
 		if err != nil {
 			logError(ctx, err, "rsyncProjectS3", "Failed relative path")
